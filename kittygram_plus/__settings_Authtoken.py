515256de36b64ff5279591ba4c957d1032a35b6b
Sergei Polikarpov <darmidont1970@gmail.com>
2025-11-24 13:49:01 +0400

    1.3

--- kittygram_plus/__settings_Authtoken.py
+++ kittygram_plus/__settings_Authtoken.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from datetime import timedelta
 
 from pathlib import Path
 
@@ -39,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
+    'rest_framework.authtoken',
     'cats.apps.CatsConfig',
 ]
 
@@ -130,16 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated', 
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
-
-SIMPLE_JWT = {
-    # Устанавливаем срок жизни токена
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'AUTH_HEADER_TYPES': ('Bearer',),
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
--- kittygram_plus/__urls_Authtoken.py
+++ kittygram_plus/__urls_Authtoken.py
@@ -13,9 +13,5 @@ router.register(r'mycats', LightCatViewSet, basename='lightcat')
 
 urlpatterns = [
     path('', include(router.urls)),
-    # Djoser создаст набор необходимых эндпоинтов.
-    # базовые, для управления пользователями в Django:
-    path('auth/', include('djoser.urls')),
-    # JWT-эндпоинты, для управления JWT-токенами:
-    path('auth/', include('djoser.urls.jwt')),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
